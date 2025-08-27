1e0a796727b779f9fe605495b9b2eadb5f004a1c
Lasitha2001 <lasithamuthukumarana2022@gmail.com>
2025-08-27 22:20:46 +0530

    install reactjs, tailwind and create the Estate Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-amber-200 font-bold text-6xl'>Welcome to the Estate Project</h1>
+    </div>
+  )
+}
+
+export default App
