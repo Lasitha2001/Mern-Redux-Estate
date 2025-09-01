18868293ca4f10175529ed569a94ff621bcad771
Lasitha2001 <lasithamuthukumarana2022@gmail.com>
2025-09-01 20:37:40 +0530

    Create Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import About from './Pages/About'
 
 function App() {
   return (
-    <div>
-      <h1 className='text-amber-200 font-bold text-6xl'>Welcome to the Estate Project</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
